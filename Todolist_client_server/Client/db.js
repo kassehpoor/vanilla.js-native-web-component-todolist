409f4958155949f02648291378929e51bfe13979
kassehpoor <kasehpoor.k@gmail.com>
2019-05-15 20:09:40 +0430

    v0.0 fix server

--- Todolist_client_server/Client/db.js
+++ Todolist_client_server/Client/db.js
@@ -5,28 +5,31 @@ var db = (function () {
         setModel:setModelToServer,
     };
     
-    function getModelFromServer(){
+    function getModelFromServer(callback){
+        let url = '/read/';
         console.log('I am getModelFromServer function....');
         let xhr = new XMLHttpRequest();
         xhr.open("GET",url,false);
         xhr.onreadystatechange = function (){
             var value;
-            let url = '/read/';
+            
             if (this.readyState == 4 && this.status == 200){
                 value = this.responseText;
                 value.forEeach((obj)=>{console.log(obj);});
             };
+
+            xhr.send(null);
+            console.log('I want to read....');
+            if (!value) return callback(null);
+            callback(JSON.parse(value))
         };
-        xhr.send(null);
-        console.log('I want to read....');
-        if (!value) return null;
-        return JSON.parse(value);
     };
 
     function setModelToServer(value){
+        let url = '/write/';
         let xhr = new XMLHttpRequest();
         console.log('I am setModelToerver function....');
-        let url = '/write/';
+        
         xhr.open("POST",url,true);
         xhr.setRequestHeader('Content-Type','application/json');
         xhr.send(JSON.stringify(value));
@@ -36,7 +39,9 @@ var db = (function () {
 
 
 
-
+getModelFromServer(function(value){
+    //use value
+})
 
 /*
 //---------------------------------------------------------
